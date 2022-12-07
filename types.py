949762aabc3efb03cd0eb140b7e3eb9106e8bbd5
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-12-07 11:08:47 +0600

    types done

---
+++ types.py
@@ -0,0 +1,32 @@
+# Text Type
+text = "Hello World!"
+
+# Numeric Types
+a = 4 #int
+b = 5.33 #float
+c = 8j #complex
+
+# Sequenec Types
+fruits = ["apple", "mango", "banana"] #list
+cars = ("audi", "volvo", "toyota") #tuple
+r = range(6) #range
+
+# Mapping Type
+person = {"name" : "Jack", "age" : "24"} #dict
+
+# Set Types
+veggies = {"carrot", "tomato", "onion"} #set
+frozen = frozenset({"python", "java", "c++"}) #frozenset
+
+# Boolean Types
+excited = True #bool
+
+# Binary Types
+x = b"Hey!" #byte
+y = bytearray(7) #bitearray
+z = memoryview(bytes(7)) #memoryview
+
+# None Type
+none = None #none
+
+print(none, type(none))
